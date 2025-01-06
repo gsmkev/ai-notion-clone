ad6bac7d1c66690fe14af0bbb54b4c4c5c32c2a9
gsmkev <gsmkev@gmail.com>
2025-01-06 10:43:24 -0300

    feat(ui): update main page to include a prompt for creating a new document with an icon

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,10 @@
+import { ArrowLeftCircle } from "lucide-react";
+
 export default function Page() {
-	return <main></main>;
+	return (
+		<main className="flex space-x-2 items-center animate-pulse">
+			<ArrowLeftCircle size={36} />
+			<h1 className="font-bold">Get started by creating a new document</h1>
+		</main>
+	);
 }
