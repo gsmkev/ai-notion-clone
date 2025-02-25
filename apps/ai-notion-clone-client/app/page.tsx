b892cba4b836bbd516225529cdbac336dbf14b39
gsmkev <gsmkev@gmail.com>
2025-02-25 07:48:17 -0300

    feat(auth): integrate Clerk for authentication
    - Implemented login in the Header using Clerk's SignInButton and UserButton

--- apps/ai-notion-clone-client/app/page.tsx
+++ apps/ai-notion-clone-client/app/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col justify-center items-center h-screen">
+		<main className="">
 			<h1>AI Notion Clone</h1>
 			<Button>Hello World</Button>
-		</div>
+		</main>
 	);
 }
