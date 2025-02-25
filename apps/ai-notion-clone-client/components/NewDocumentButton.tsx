fe485a4e975a80055f72f1550bcb6e537319f543
gsmkev <gsmkev@gmail.com>
2025-02-25 10:29:32 -0300

    feat(document): implement createNewDocument action and update NewDocumentButton for async document creation

--- apps/ai-notion-clone-client/components/NewDocumentButton.tsx
+++ apps/ai-notion-clone-client/components/NewDocumentButton.tsx
@@ -1,6 +1,32 @@
+"use client";
+
+import { useTransition } from "react";
 import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
+import { createNewDocument } from "@/actions/actions";
 
 function NewDocumentButton() {
-	return <Button>New Document</Button>;
+	const [isPending, startTransition] = useTransition();
+	const router = useRouter();
+
+	const handleCreateNewDocument = () => {
+		startTransition(async () => {
+			const { docId } = await createNewDocument();
+			router.push(`/doc/${docId}`);
+		});
+	};
+
+	return (
+		<Button onClick={handleCreateNewDocument} disabled={isPending}>
+			{isPending ? (
+				<div className="animate-spin">
+					<Loader2 size={40} />
+				</div>
+			) : (
+				"New Document"
+			)}
+		</Button>
+	);
 }
 export default NewDocumentButton;
