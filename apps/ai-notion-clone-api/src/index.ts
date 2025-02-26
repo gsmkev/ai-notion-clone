5dbec415d8f6069795806de40aecaf5b4782f0c7
gsmkev <gsmkev@gmail.com>
2025-02-26 08:46:04 -0300

    feat(api): integrate Hono framework for improved routing and add ai translation endpoint

--- apps/ai-notion-clone-api/src/index.ts
+++ apps/ai-notion-clone-api/src/index.ts
@@ -1,18 +1,45 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+export type Bindings = {
+	AI: Ai;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(
+	'/*',
+	cors({
+		origin: '*',
+		allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'],
+		allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT'],
+		exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true,
+	})
+);
+
+app.get('/', async () => {
+	return new Response('Hello World');
+});
+
+app.post('/translate/', async (c) => {
+	const { data, source, target } = await c.req.json<{ data: string; source: string; target: string }>();
+
+	// Generate a summary of the text
+	const summary = await c.env.AI.run('@cf/facebook/bart-large-cnn', {
+		input_text: data,
+		max_length: 1000,
+	});
+
+	// Translate the summary to the target language
+	const translate = await c.env.AI.run('@cf/meta/m2m100-1.2b', {
+		text: summary.summary,
+		source_lang: source,
+		target_lang: target,
+	});
+
+	return new Response(JSON.stringify(translate));
+});
+
+export default app;
